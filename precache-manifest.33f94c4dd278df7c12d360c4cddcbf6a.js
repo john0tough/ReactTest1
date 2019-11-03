bf4d945e3292719dc205ce4eb0756b262a40604b
Gio <gio.garciap@gmail.com>
2019-11-02 20:01:00 -0600

    Updates

---
+++ precache-manifest.33f94c4dd278df7c12d360c4cddcbf6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd64e986701444621acbdc02edff92d7",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "5516f72124c13c411943",
+    "url": "/react-test/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4eefe993c21c2e3bd153",
+    "url": "/react-test/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "5516f72124c13c411943",
+    "url": "/react-test/static/js/2.741e9bfd.chunk.js"
+  },
+  {
+    "revision": "4eefe993c21c2e3bd153",
+    "url": "/react-test/static/js/main.6a2fc578.chunk.js"
+  },
+  {
+    "revision": "6c1f8fe04ce917219e47",
+    "url": "/react-test/static/js/runtime-main.dbe67276.js"
+  }
+]);
